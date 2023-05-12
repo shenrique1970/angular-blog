1ce910d43f4898f9eff399f362227db565c61993
Shenrique1970 <shvribeiro@hotmail.com>
2023-05-12 09:00:24 -0300

    dinamica doas informações

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
     {
-        "id": 1,
-        "photo":"aaaa",
-        "title":"saiu novo filme",
-        "description":"ola"
+        "id": "1",
+        "title":"Angular 15",
+        "description":"Estou usando Angular 15",
+        "photoCover": "http://localhost:4200/assets/angular.jpeg"
     },
     {
-        "id": 2,
-        "photo":"aaaa",
-        "title":"saiu novo filme",
-        "description":"ola"
+        "id": "2",
+        "title":"Serie antiga O Tunel do Tempo",
+        "description":"ola, curta series antigas",
+        "photoCover": "http://localhost:4200/assets/series.jpeg"
+    },
+    {
+        "id": "3",
+        "title":"Serie Arquuivo x a melhor serie",
+        "description":"ola, curta series antigas",
+        "photoCover": "http://localhost:4200/assets/arquivox.jpeg"
+    },
+    {
+        "id": "4",
+        "title":"Serie antiga inesquecivel Ultraman Jack",
+        "description":"ola, curta series antigas",
+        "photoCover": "http://localhost:4200/assets/ultraman.jpg"
     }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {dataFake} from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -6,14 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-  photoCover:string = "http://localhost:4200/assets/angular.jpeg"
-  contentTitle:string = "MINHA NOTICIA DE EXEMPLO"
-  contentDescription:string = "BLA BLA BLA BLA BLA"
+  photoCover:string = ""
+  contentTitle:string = ""
+  contentDescription:string = ""
+  private id:string | null = "0"
 
+  constructor(
+    private route:ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-   
+    this.route.paramMap.subscribe( value =>
+      this.id = value.get("id")
+    )
+    this.setValuesToComponent(this.id)
+  
   }
   
+  setValuesToComponent(id:string | null) {
+    const result = dataFake.filter(article => article.id == id)[0]
+    
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
+  }
+ 
 
 }
